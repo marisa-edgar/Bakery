7047f9a69f96aeddac7aed998480c20ca52d467f
marisa-edgar <marisaedgar1212@gmail.com>
2022-02-25 09:24:16 -0800

    WIP planning

---
+++ Bakery/Models/Bread.cs
@@ -0,0 +1,12 @@
+namespace ToDoList.Models
+{
+  public class Item
+  {
+    public string Description { get; }
+
+    public Item(string description)
+    {
+      Description = description;
+    }
+  }
+}
---
+++ Bakery/Program.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+namespace Bakery.Models
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      
+    }
